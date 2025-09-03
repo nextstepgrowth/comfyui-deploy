892ffb4c26f27dcb4a32e18d8f74d88f968953c5
clogic <choiseungil29@gmail.com>
2025-09-04 00:32:27 +0900

    neon 제거

--- web/src/db/db.ts
+++ web/src/db/db.ts
@@ -1,30 +1,12 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { drizzle as neonDrizzle } from "drizzle-orm/neon-serverless";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const isDevContainer = process.env.REMOTE_CONTAINERS !== undefined;
-
-// if we're running locally
-if (process.env.VERCEL_ENV !== "production") {
-  // Set the WebSocket proxy to work with the local instance
-  if (isDevContainer) {
-    // Running inside a VS Code devcontainer
-    neonConfig.wsProxy = (host) => "host.docker.internal:5481/v1";
-  } else {
-    // Not running inside a VS Code devcontainer
-    neonConfig.wsProxy = (host) => `${host}:5481/v1`;
-  }
-  // Disable all authentication and encryption
-  neonConfig.useSecureWebSocket = false;
-  neonConfig.pipelineTLS = false;
-  neonConfig.pipelineConnect = false;
-}
-
-export const db = neonDrizzle(
+export const db = drizzle(
   new Pool({
     connectionString: process.env.POSTGRES_URL,
   }),
   {
     schema,
-  },
+  }
 );
